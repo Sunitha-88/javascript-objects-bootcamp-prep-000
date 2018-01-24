0f4fcca824c3c8bccc6c93e9ea651c3f60e6d079
Suni <suni.suni571@gmail.com>
2018-01-24 13:03:27 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,8 +5,8 @@ function updatePlaylist(playlist, artistName, songTitle){
 
 	return Object.assign({},playlist,{[artistName]:songTitle});
 	}
-function removeFromPlaylist(playlist1,artistName){
-	var ans=delete playlist.artistName;
-	console.log(ans);
-  return playlist1;
+function removeFromPlaylist(playlist,artistName){
+	delete playlist[artistName];
+	
+  return playlist;
 	}
